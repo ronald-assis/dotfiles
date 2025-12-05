ec95233494a75d5371f6ed5e3558e6a2c137712c
ronald_assis <ronaldassis71@gmail.com>
2025-12-04 21:39:48 -0300

    feat(nvim): add custom copilot plugin initialization

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -2,3 +2,4 @@ vim.opt.number = true
 vim.opt.cursorline = true
 vim.opt.relativenumber = false
 vim.opt.shiftwidth = 2
+vim.opt.guifont = "monospace:h14"
--- .config/nvim/lua/plugins/copilot-chat.lua
+++ .config/nvim/lua/plugins/copilot-chat.lua
@@ -20,5 +20,18 @@ return {
       { "<leader>co", ":CopilotChatOptimize<CR>",      mode = "v", desc = "Optimize selection" },
       { "<leader>cm", ":CopilotChatCommit<CR>",        mode = "n", desc = "Generate Commit Message" },
     },
+    config = function()
+      -- Disable default tab mapping
+      require("CopilotChat").setup({})
+      vim.g.copilot_no_tab_map = true
+      vim.api.nvim_set_keymap("i", "<Tab>", 'copilot#Accept("<CR>")', { silent = true, expr = true })
+
+      -- Map Ctrl-] and Ctrl-[ to navigate suggestions
+      vim.api.nvim_set_keymap("i", "<C-]>", "<Plug>(copilot-next)", { silent = true })
+      vim.api.nvim_set_keymap("i", "<C-[>", "<Plug>(copilot-previous)", { silent = true })
+
+      -- Assume mappings are set
+      vim.g.copilot_assume_mapped = true
+    end,
   },
 }
